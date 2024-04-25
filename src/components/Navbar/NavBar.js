57de9582b901b16d838cee0db7af7ee66fc51805
abdularhamkhanzada <abdularhamkhan02@gmail.com>
2024-04-25 20:47:52 +0500

    -translatex-4 navbar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -56,7 +56,7 @@ const NavBar = () => {
                         <NavLinks />
                     </div>
 
-                    <div className={`fixed transition-transform duration-300 ease-in-out transit flex justify-center left-0 w-full h-auto rounded-md p-24 bg-white lg:hidden shadow-xl top-14 ${  isOpen ? "block" : "hidden" } `}>
+                    <div className={`fixed transition-transform duration-300 ease-in-out transit flex justify-center left-0 w-full h-auto rounded-md p-24 bg-white lg:hidden shadow-xl top-14 lg:-translate-x-4 ${  isOpen ? "block" : "hidden" } `}>
                         <div className='flex flex-col space-y-6 lg:-translate-x-4'>
                             <NavLinks />
                         </div>                                                
